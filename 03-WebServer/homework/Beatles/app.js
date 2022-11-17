77a89a25f29861367e3a9dc2a0e383dedeb83334
Nelson Giovanny Vasquez Blanco <gioh2020@gmail.com>
2022-11-17 09:43:10 -0500

    I finished homework 03

--- 03-WebServer/homework/Beatles/app.js
+++ 03-WebServer/homework/Beatles/app.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var fs   = require('fs');
+const { url } = require('inspector');
 
 var beatles=[{
   name: "John Lennon",
@@ -22,3 +23,52 @@ var beatles=[{
   profilePic:"http://cp91279.biography.com/BIO_Bio-Shorts_0_Ringo-Starr_SF_HD_768x432-16x9.jpg"
 }
 ]
+
+http
+.createServer((req, res) => {
+if(req.url === "/"){
+  const indx = fs.readFileSync("./index.html", "utf-8");
+  res.writeHead((200),{"content-type":"text/html"})
+  res.end(JSON.stringify(indx))
+}
+const urlname= req.url.split("/")
+if(urlname.length < 3){
+  const beatleName = urlname.pop().replace("%20", " ") 
+  const beatle = beatles.filter(elemnt => elemnt.name === beatleName)[0];
+  console.log(beatle)
+  let template = fs.readFileSync("./beatle.html", "utf-8");
+  
+  template = template.replace("{name}", beatle.name);
+  template = template.replace("{birthdate}", beatle.birthdate);
+  template = template.replace("{profilePic}", beatle.profilePic);
+  console.log(template)
+  res.writeHead((200),{"content-type": "text/html"})
+  return res.end(JSON.stringify(template))
+
+
+}
+
+  if(req.url === "/api"){
+ 
+  res.writeHead((200),{"content-type":"aplication/jason"})
+  return res.end(JSON.stringify(beatles)
+  )};
+
+  const name = req.url.split("/").pop().replace("%20", " ");
+  console.log(name)
+  
+
+  
+
+  if(req.url.includes("/api")&& name){
+    const beatle = beatles.filter(elemnt => elemnt.name === name);
+    console.log(beatle)
+    if(!beatle.length){ return res.writeHead((404)).end('not found 404')}
+
+    res.writeHead((200),{"content-type":"aplication/jason"})
+    return res.end(JSON.stringify(beatle))
+  }
+  
+
+}).listen(3000, "localhost")
+
--- 03-WebServer/homework/showByName/app.js
+++ 03-WebServer/homework/showByName/app.js
@@ -2,3 +2,25 @@ var fs  = require("fs")
 var http  = require("http")
 
 // Escribí acá tu servidor
+http
+.createServer((req, res) => {
+  let name = req.url.slice(1)
+  console.log(name)
+  const files = fs.readdirSync('./images');
+
+
+  for(const file of files){
+    if(file.includes(name)){
+        res.writeHead(200,{"content-type": "image/jgp"})
+        const imagen = fs.readFileSync(`./images/${name}_doge.jpg`)
+        return res.end(imagen)
+    }
+  }
+
+  return res.writeHead((404)).end('not found 404')
+
+  
+
+
+
+}).listen(3001, "localhost")
